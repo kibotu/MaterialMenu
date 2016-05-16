2ed0455dd4a510dabd7dea24ec7e617b319054a1
Jan Rabe <cloudgazer3d@gmail.com>
2016-05-16 22:50:57 +0200

    added a menu item presenter to manage menu items within the recyclerview

--- lib/src/main/java/net/kibotu/android/materialmenu/MenuItem.java
+++ lib/src/main/java/net/kibotu/android/materialmenu/MenuItem.java
@@ -1,51 +1,127 @@
 package net.kibotu.android.materialmenu;
 
 
+import android.support.annotation.DrawableRes;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.annotation.StringRes;
+import android.support.v4.app.Fragment;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
 
-import net.kibotu.android.materialmenu.internal.LogTag;
+import net.kibotu.android.recyclerviewpresenter.Presenter;
 
-//import android.support.v4.app.Fragment;
+import static net.kibotu.android.materialmenu.FragmentExtensions.replaceFragmentAddToBackStack;
+import static net.kibotu.android.materialmenu.MaterialMenu.fragmentContainerId;
 
 /**
  * Created by Nyaruhodo on 20.02.2016.
  */
 public class MenuItem {
-//
-//    @StringRes
-//    final int name;
-//
-//    @DrawableRes
-//    final int icon;
+
+    @StringRes
+    int label;
+
+    @DrawableRes
+    int icon;
+
+    @StringRes
+    int analyticsTag;
 
     @Nullable
-    private Class<? extends LogTag> fragment;
-//
-//    public <T extends Fragment & LogTag> MenuItem(@StringRes final int name, @DrawableRes final int icon, @Nullable final Class<T> fragment) {
-//        this.name = name;
-//        this.icon = icon;
-//        this.fragment = fragment;
-//    }
+    View.OnClickListener action;
 
-//    @SuppressWarnings("unchecked")
-//    @NonNull
-//    public <T extends Fragment & LogTag> T getFragment() {
-//        T result = null;
-//        try {
-//            result = (T) fragment.getConstructors()[0].newInstance();
-//        } catch (final InstantiationException e) {
-//            e.printStackTrace();
-//        } catch (final IllegalAccessException e) {
-//            e.printStackTrace();
-//        } catch (final InvocationTargetException e) {
-//            e.printStackTrace();
-//        } catch (final NullPointerException e) {
-//            e.printStackTrace();
-//        }
-//
-//        if (result == null)
-//            throw new IllegalArgumentException("fragment has no empty constructor");
-//
-//        return result;
-//    }
+    @NonNull
+    Class<? extends Presenter<MenuItem, ? extends RecyclerView.ViewHolder>> presenter = MenuItemPresenter.class;
+
+    public MenuItem() {
+    }
+
+    @StringRes
+    public int getLabel() {
+        return label;
+    }
+
+    public MenuItem setLabel(@StringRes final int label) {
+        this.label = label;
+        return this;
+    }
+
+    @DrawableRes
+    public int getIcon() {
+        return icon;
+    }
+
+    public MenuItem setIcon(@DrawableRes final int icon) {
+        this.icon = icon;
+        return this;
+    }
+
+    @StringRes
+    public int getAnalyticsTag() {
+        return analyticsTag;
+    }
+
+    public MenuItem setAnalyticsTag(@StringRes final int analyticsName) {
+        this.analyticsTag = analyticsName;
+        return this;
+    }
+
+    @NonNull
+    public Class<? extends Presenter<MenuItem, ? extends RecyclerView.ViewHolder>> getPresenter() {
+        return presenter;
+    }
+
+    public <T extends Presenter<MenuItem, ? extends RecyclerView.ViewHolder>> MenuItem setPresenter(@NonNull final Class<T> presenter) {
+        this.presenter = presenter;
+        return this;
+    }
+
+    @Nullable
+    public View.OnClickListener getAction() {
+        return action;
+    }
+
+    public MenuItem setAction(@Nullable final View.OnClickListener action) {
+        this.action = action;
+        return this;
+    }
+
+    public <T extends Fragment & FragmentTag> MenuItem setOpenFragmentAction(@NonNull final Class<T> fragmentClass) {
+        return setAction((v) -> replaceFragmentAddToBackStack(fragmentContainerId(), fragmentClass));
+    }
+
+    @Override
+    public boolean equals(Object o) {
+
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        MenuItem menuItem = (MenuItem) o;
+
+        if (label != menuItem.label) return false;
+        if (icon != menuItem.icon) return false;
+        if (analyticsTag != menuItem.analyticsTag) return false;
+        return action != null ? action.equals(menuItem.action) : menuItem.action == null;
+
+    }
+
+    @Override
+    public int hashCode() {
+        int result = label;
+        result = 31 * result + icon;
+        result = 31 * result + analyticsTag;
+        result = 31 * result + (action != null ? action.hashCode() : 0);
+        return result;
+    }
+
+    @Override
+    public String toString() {
+        return "MenuItem{" +
+                "label=" + label +
+                ", icon=" + icon +
+                ", analyticsTag=" + analyticsTag +
+                ", action=" + action +
+                '}';
+    }
 }
--- lib/src/main/java/net/kibotu/android/materialmenu/MenuItemPresenter.java
+++ lib/src/main/java/net/kibotu/android/materialmenu/MenuItemPresenter.java
@@ -1,7 +1,107 @@
 package net.kibotu.android.materialmenu;
 
+import android.content.Context;
+import android.support.annotation.LayoutRes;
+import android.support.annotation.NonNull;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.bumptech.glide.Glide;
+
+import net.kibotu.android.recyclerviewpresenter.BaseViewHolder;
+import net.kibotu.android.recyclerviewpresenter.Presenter;
+import net.kibotu.android.recyclerviewpresenter.PresenterAdapter;
+import net.kibotu.materialmenu.R;
+
+import java.util.Random;
+
 /**
  * Created by Nyaruhodo on 15.05.2016.
  */
-public class MenuItemPresenter {
+public class MenuItemPresenter extends Presenter<MenuItem, MenuItemPresenter.ViewHolder> {
+
+    private final int id;
+
+    public MenuItemPresenter(@NonNull PresenterAdapter<MenuItem> presenterAdapter) {
+        super(presenterAdapter);
+
+        id = new Random().nextInt(Integer.MAX_VALUE);
+    }
+
+    @Override
+    protected int getLayout() {
+        return R.layout.menu_item;
+    }
+
+    @NonNull
+    @Override
+    protected ViewHolder createViewHolder(@LayoutRes int layout, @NonNull ViewGroup viewGroup) {
+        return new ViewHolder(layout, viewGroup);
+    }
+
+    @Override
+    public void bindViewHolder(@NonNull ViewHolder viewHolder, @NonNull MenuItem item, int position) {
+        final Context context = viewHolder.itemView.getContext();
+
+        if (item.icon != 0)
+            Glide.with(context)
+                    .load(item.icon)
+                    .into(viewHolder.icon);
+
+        viewHolder.label.setText(context.getString(item.label));
+
+        if (item.action != null)
+            viewHolder.itemView.setOnClickListener(item.action::onClick);
+
+        //        viewHolder.textView.setText(item.getLocalizedName());
+//        viewHolder.imageView.setImageResource(item.getIconId());
+//        viewHolder.itemView.setOnClickListener(v -> {
+//            MainMenuProvider.provide().closeDrawers();
+//
+//            if (item.hasAction())
+//                item.getAction().run();
+//            else if (item.getFragmentClass() != null)
+//                replaceToBackStackBySlidingHorizontally(item.getFragment());
+//        });
+    }
+
+    public static class ViewHolder extends BaseViewHolder {
+
+        @NonNull
+        ImageView icon;
+
+        @NonNull
+        TextView label;
+
+        public ViewHolder(@LayoutRes int layout, @NonNull ViewGroup parent) {
+            super(layout, parent);
+
+            icon = (ImageView) itemView.findViewById(R.id.icon);
+            label = (TextView) itemView.findViewById(R.id.label);
+        }
+
+        @Override
+        public void onViewDetachedFromWindow() {
+            super.onViewDetachedFromWindow();
+            if (itemView != null)
+                itemView.setOnClickListener(null);
+        }
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        MenuItemPresenter that = (MenuItemPresenter) o;
+
+        return id == that.id;
+
+    }
+
+    @Override
+    public int hashCode() {
+        return id;
+    }
 }
